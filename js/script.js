828a0a4a79e936bc850d9177ac6012de47c05d6c
Kyrychenko-90 <makedonski.ua@gmail.com>
2022-12-09 21:30:58 +0200

    add task6

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let userInput = prompt("Введите слова или цифры");
+console.log(userInput);
+
+const input = [];
+
+input.push(userInput);
+console.log(input);
+
+while(userInput) {
+    userInput = prompt("Введите слова или цифры");
+    if(userInput) {
+        input.push(userInput);
+    }
+    console.log(input);
+}
+
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+input.sort(compareNumbers);
+console.log(input);
+
+input.splice(2,3);
+console.log(input);
+
